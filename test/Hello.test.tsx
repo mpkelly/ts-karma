38e6536f8dd3351cd9ae7ad97f7dd0bdc989902a
Michael Kelly <Mike@Michaels-MacBook-Air.local>
2017-08-07 14:42:38 +0800

    fix test name

--- test/Hello.test.tsx
+++ test/Hello.test.tsx
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import Hello from "../src/Hello";
 
 describe("Hello", () => {
-  it('renders three <Hello /> components', () => {
+  it('renders a <Hello /> component', () => {
     const wrapper = mount(<Hello name="Mike" />);
     expect(wrapper.find("h1").text()).to.contain('Hello Mike');
   });
